3378e4b7a4528d0b8a86ddb0fb9e033a19fd5dcd
Mateus <mateus.castro2011@gmail.com>
2025-10-16 19:42:27 -0300

    feat: add MinhasPropostas route and PropostaType interface

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import App from './App.tsx'
 import Login from './Login.tsx'
 import Detalhes from './Detalhes.tsx'
 import MinhasNoticias from './MinhasNoticias.tsx'
+import MinhasPropostas from './MinhasPropostas.tsx'
 import PainelAdmin from './PainelAdmin.tsx'
 import AreaCliente from './AreaCliente.tsx'
 import Dashboard from './Dashboard.tsx'
@@ -24,6 +25,7 @@ const rotas = createBrowserRouter([
       { path: 'cadastro', element: <Cadastro /> },
       { path: 'noticia/:id', element: <Detalhes /> },
       { path: 'minhas-noticias', element: <MinhasNoticias /> },
+      { path: 'minhas-propostas', element: <MinhasPropostas /> },
       { path: 'area-cliente', element: <AreaCliente /> },
       { path: 'admin', element: <PainelAdmin /> },
       { path: 'dashboard', element: <Dashboard /> },
---
+++ src/utils/PropostaType.ts
@@ -0,0 +1,7 @@
+export interface PropostaType {
+  id: number;
+  titulo: string;
+  descricao: string;
+  status: string;
+  data: string;
+}
